26b1e1b830464979ce20f22d7a320fed0a4d71bb
tcaror <tcaror@gmail.com>
2023-09-07 20:42:48 -0700

    coded a list on chars in FS

---
+++ main.py
@@ -0,0 +1,40 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    letter_text = print_letters(text)
+    print(letter_text)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_character_sums(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def print_letters(text):
+    char = get_chars_dict(text)
+    for key,value in char.items():
+        print(f"The {key} charater was found {value} times")
+
+main()
